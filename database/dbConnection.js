4f6dca6c3301298049531c04f5d0eac5fc62d9cc
VishalGond7068 <vishalkumar740813@gmail.com>
2025-04-14 03:19:49 +0530

    Updated user controller, DB connection, and JWT token utilities

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = ()=>{
     mongoose.connect(process.env.MONGO_URI, {
-        dbName: "MERN_STACK_JOB_SEEKING",
+        dbName: "MERN_STACK_JOB_SEEKING_WEBAPP",
     })
     .then(()=>{
         console.log('Connected to database!');
